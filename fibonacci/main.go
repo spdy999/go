faa4039fcb8764ab15657a6c25340eba8b86a7fc
spdy999 <1a1a1a.262626.0010@gmail.com>
2023-09-16 19:04:16 +0700

    add channel

--- fibonacci/main.go
+++ fibonacci/main.go
@@ -1,14 +1,34 @@
 package main
 
-func fibonacci(max int) {
+import "fmt"
+
+func fibonacci(max int, ch chan int) {
 	fib := make([]int, max)
 	fib[0] = 0
 	fib[1] = 1
 
+	ch <- fib[0]
+	ch <- fib[1]
+
 	for i := 2; i < max; i++ {
 		fib[i] = fib[i-1] + fib[i-2]
+		ch <- fib[i] // send(?)
 	}
+	close(ch)
 }
 
 func main() {
+	ch := make(chan int)
+	go fibonacci(6, ch)
+
+	for msg := range ch {
+		fmt.Println(msg)
+		// result
+		// 0
+		// 1
+		// 1
+		// 2
+		// 3
+		// 5
+	}
 }
